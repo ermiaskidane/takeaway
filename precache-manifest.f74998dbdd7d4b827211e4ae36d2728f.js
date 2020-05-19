b6cd0cc205b6f6b315db5345e804c4ddd6da5381
ermias kidane <hadeklte21@gmail.com>
2020-05-19 05:08:34 +0100

    Updates

---
+++ precache-manifest.f74998dbdd7d4b827211e4ae36d2728f.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b18dec9513c56c8169b378b9885e2a7",
+    "url": "/takeaway/./index.html"
+  },
+  {
+    "revision": "b4383a0aa8d1b226ff03",
+    "url": "/takeaway/./static/css/main.f8dde05b.chunk.css"
+  },
+  {
+    "revision": "72728d6dfea926074006",
+    "url": "/takeaway/./static/js/2.81189d00.chunk.js"
+  },
+  {
+    "revision": "b4383a0aa8d1b226ff03",
+    "url": "/takeaway/./static/js/main.d129ff11.chunk.js"
+  },
+  {
+    "revision": "e5055844037e39629681",
+    "url": "/takeaway/./static/js/runtime-main.8cd22f12.js"
+  },
+  {
+    "revision": "53f2697ddbd56c06e444b7de9030290c",
+    "url": "/takeaway/./static/media/burger-1 (Custom).53f2697d.jpg"
+  },
+  {
+    "revision": "dce8fbf6e516b76db012e41dd600bdfd",
+    "url": "/takeaway/./static/media/burger-2 (Custom).dce8fbf6.jpg"
+  },
+  {
+    "revision": "b7626a68e3b16f340e68a3989c336c25",
+    "url": "/takeaway/./static/media/burger-3 (Custom).b7626a68.jpg"
+  },
+  {
+    "revision": "ed5d8dd457ae5bac257337c1650d5b44",
+    "url": "/takeaway/./static/media/chillie-fries.ed5d8dd4.jpg"
+  },
+  {
+    "revision": "a3dda1bc81be44d1583a9ae7a0dfccc9",
+    "url": "/takeaway/./static/media/fried-chicken (Custom).a3dda1bc.jpg"
+  },
+  {
+    "revision": "e4ca82238e43d08c2e60e2931fddff38",
+    "url": "/takeaway/./static/media/fries-meat (Custom).e4ca8223.jpg"
+  },
+  {
+    "revision": "3ccc042c719bb1fe173a081731656fc5",
+    "url": "/takeaway/./static/media/hand-taco.3ccc042c.jpg"
+  },
+  {
+    "revision": "2bf7c1243e9dcf1bebadbf8d18c8006d",
+    "url": "/takeaway/./static/media/pizza-3.2bf7c124.jpg"
+  },
+  {
+    "revision": "30d3a5a9b5b1be5cd5c64723d130d9e2",
+    "url": "/takeaway/./static/media/pizza-4 (Custom).30d3a5a9.jpg"
+  },
+  {
+    "revision": "269e51f9ee99d3736e64ae4c134c7a6f",
+    "url": "/takeaway/./static/media/pizza-5 (Custom).269e51f9.jpg"
+  },
+  {
+    "revision": "096fb2dc53edae6578857a26e96dee8d",
+    "url": "/takeaway/./static/media/pizza-5-Large.096fb2dc.jpg"
+  },
+  {
+    "revision": "91bba70bf781d101f67fb0290179a14d",
+    "url": "/takeaway/./static/media/pizza-meat-1.91bba70b.jpg"
+  },
+  {
+    "revision": "c977f7c79bb725262a6d4b6882a6f33a",
+    "url": "/takeaway/./static/media/pizza-pepperoni (Custom).c977f7c7.jpg"
+  },
+  {
+    "revision": "c2f1d3f96796a7e518d9aa586090e04b",
+    "url": "/takeaway/./static/media/pizza-veg (Custom).c2f1d3f9.jpg"
+  },
+  {
+    "revision": "9b631f93ed364870788c8aad07c83fd1",
+    "url": "/takeaway/./static/media/shop-a.9b631f93.jpg"
+  },
+  {
+    "revision": "f0a8d1546ebdfd2503d0ab02430e9120",
+    "url": "/takeaway/./static/media/shop-b (Custom).f0a8d154.jpg"
+  },
+  {
+    "revision": "07751e9e36006a836de1c061adb4cffd",
+    "url": "/takeaway/./static/media/shop-b.07751e9e.jpg"
+  },
+  {
+    "revision": "c32102b822d66bbf535811af2c6648ca",
+    "url": "/takeaway/./static/media/shop-burger-1.c32102b8.jpg"
+  },
+  {
+    "revision": "902a996ac829ae05d50436d65042e191",
+    "url": "/takeaway/./static/media/shop-burger-2.902a996a.jpg"
+  },
+  {
+    "revision": "ff28626ec05d75f6c3ff5faaede90772",
+    "url": "/takeaway/./static/media/shop-burrito-1.ff28626e.jpg"
+  },
+  {
+    "revision": "d80165defc93ba2529300793f9ede6bf",
+    "url": "/takeaway/./static/media/shop-c (Medium).d80165de.jpg"
+  },
+  {
+    "revision": "a3f48a042a0ceb7e83dd01c35e440630",
+    "url": "/takeaway/./static/media/sprite.a3f48a04.svg"
+  },
+  {
+    "revision": "ac47e85fe057bc81871b8fc4183d941b",
+    "url": "/takeaway/./static/media/taco-1 (Custom).ac47e85f.jpg"
+  },
+  {
+    "revision": "0c2ee36e4fa8e7a04c2bf34d4ba45708",
+    "url": "/takeaway/./static/media/taco-2 (Custom).0c2ee36e.jpg"
+  },
+  {
+    "revision": "7d02e1ea7e245f5fcdf0ae44a859e011",
+    "url": "/takeaway/./static/media/wrap-2 (Custom).7d02e1ea.jpg"
+  }
+]);
--- src/components/backdrop/backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./backdrop.scss";
-
-const backdrop = props => {
-  return <div className="backdrop" onClick={props.open}></div>;
-};
-
-export default backdrop;
